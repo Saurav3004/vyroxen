ef269a2ec9b3a076540e49b54402f6279e068920
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-06-02 09:00:11 +0530

    Tool-Executor Part Completed

---
+++ ai/ai.config.ts
@@ -0,0 +1,10 @@
+import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+
+export function getAgentModel(){
+    const provider = createOpenRouter({apiKey:process.env.OPENROUTER_API_KEY});
+
+    const modelId = process.env.OPENROUTER_DEFAULT_MODEL as string;
+
+    return provider(modelId);
+}
+
